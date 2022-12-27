b1af50af1594f910ac9567fe21822efd0ca63c50
ErrorX <99178547+FakeErrorX@users.noreply.github.com>
2022-12-27 23:00:54 +0600

    Update fakewebsite.sh

--- nginx/fakewebsite.sh
+++ nginx/fakewebsite.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 #
-# 自动下载静态伪装网站到指定目录
+# Automatically download static camouflage website to specified directory
 
 red='\033[0;31m'
 green='\033[0;32m'
@@ -11,26 +11,26 @@ export PATH=$PATH:/usr/local/bin
 
 WEB_ROOT=$1
 if [ -z "$WEB_ROOT" ]; then
-    echo "用法: $0 网站root目录"
-    echo -e "${red}未输入目录值，将使用默认值: /var/www/html${plain}"
+    echo "usage: $0 Website root directory"
+    echo -e "${red}no catalog value entered，will use the default: /var/www/html${plain}"
     WEB_ROOT="/var/www/html"
-    echo -e "是否继续？[Y/n]"
-    read -p "(默认: y):" yn
+    echo -e "Whether to continue？[Y/n]"
+    read -p "(default: y):" yn
     [ -z "${yn}" ] && yn="y"
     if [[ $yn == [Yy] ]]; then
-        echo -e "${green}继续执行${plain}"
+        echo -e "${green}continue to execute${plain}"
     else
-        echo -e "${red}退出执行${plain}"
+        echo -e "${red}quit execution${plain}"
         exit 1
     fi
 fi
 
 if [ ! -d "$WEB_ROOT" ]; then
-    echo -e "${red}错误！目录不存在${plain}"
+    echo -e "${red}mistake! directory does not exist${plain}"
     exit 1
 fi
 
-# 下载静态伪装网站
+# Download Static Cloaking Sites
 # static html resource url list
 STATIC_HTML_URL_LIST=(
     "https://chomp.webflow.io/"
@@ -68,7 +68,7 @@ if [ ! -x "$(command -v wget)" ]; then
         yum install -y wget
     fi
 fi
-# 下载静态伪装网站，index.html
+# Download Static Cloaking Sites，index.html
 wget -O ${WEB_ROOT}/index.html ${STATIC_HTML_URL}
 
-echo -e "${green}下载完成，下载文件在 ${WEB_ROOT}/index.html${plain}"
+echo -e "${green}Download completed，Download file at ${WEB_ROOT}/index.html${plain}"
