390ea5d4ed3f1ebcbca5f860c23d34406277755a
ErrorX <99178547+FakeErrorX@users.noreply.github.com>
2022-12-27 22:53:41 +0600

    Update xrayr.sh
    After repairing and changing the config path, view the configuration bug

--- xrayr.sh
+++ xrayr.sh
@@ -5,8 +5,8 @@
 
 XRAYR_PATH="/opt/xrayr"
 
-DC_URL="https://raw.githubusercontent.com/tech-fever/xrayr-docker-script/main/docker-compose.yml"
-CONFIG_URL="https://raw.githubusercontent.com/tech-fever/xrayr-docker-script/main/config.yml"
+DC_URL="https://raw.githubusercontent.com/FakeErrorX/xrayr-docker-script/main/docker-compose.yml"
+CONFIG_URL="https://raw.githubusercontent.com/FakeErrorX/xrayr-docker-script/main/config.yml"
 
 red='\033[0;31m'
 green='\033[0;32m'
@@ -19,14 +19,14 @@ os_arch=""
 Get_Docker_URL="https://get.docker.com"
 GITHUB_URL="github.com"
 
-# 请填写你的面板域名，例如：https://v2board.com/
+# Please fill in your panel domain name, for example: https://v2board.com/
 PANEL_URL="https://v2board.com/"
-# 请填写面板的api key，例如：123456789
+# Please fill in the api key of the panel, for example: 123456789
 PANEL_API_KEY="your_api_key"
 
 pre_check() {
     # check root
-    [[ $EUID -ne 0 ]] && echo -e "${red}错误: ${plain} 必须使用root用户运行此脚本！\n" && exit 1
+    [[ $EUID -ne 0 ]] && echo -e "${red}错误: ${plain} The root user must be used to run this script!\n" && exit 1
 
     ## os_arch
     if [[ $(uname -m | grep 'x86_64') != "" ]]; then
@@ -50,7 +50,7 @@ install_base() {
 }
 
 install_soft() {
-    # Arch官方库不包含selinux等组件
+    # Arch official library does not contain components such as selinux
     (command -v yum >/dev/null 2>&1 && yum makecache && yum install $* selinux-policy -y) ||
         (command -v apt >/dev/null 2>&1 && apt update && apt install $* selinux-utils -y) ||
         (command -v pacman >/dev/null 2>&1 && pacman -Syu $*) ||
@@ -60,24 +60,24 @@ install_soft() {
 install() {
     install_base
 
-    echo -e "> 安装xrayr"
+    echo -e "> install xrayr"
 
     # check directory
     if [ ! -d "$XRAYR_PATH/XrayR" ]; then
         mkdir -p $XRAYR_PATH/XrayR
     else
-        echo "您可能已经安装过xrayr，重复安装会覆盖数据，请注意备份。"
-        read -e -r -p "是否退出安装? [Y/n] " input
+        echo "You may have installed xrayr before, repeated installation will overwrite data, please pay attention to backup."
+        read -e -r -p "Do you want to exit the installation? [Y/n]" input
         case $input in
         [yY][eE][sS] | [yY])
-            echo "退出安装"
+            echo "exit installation"
             exit 0
             ;;
         [nN][oO] | [nN])
-            echo "继续安装"
+            echo "continue to install"
             ;;
         *)
-            echo "退出安装"
+            echo "exit installation"
             exit 0
             ;;
         esac
@@ -87,28 +87,28 @@ install() {
     # check docker
     command -v docker >/dev/null 2>&1
     if [[ $? != 0 ]]; then
-        echo -e "正在安装 Docker"
+        echo -e "Installing Docker"
         bash <(curl -sL ${Get_Docker_URL}) >/dev/null 2>&1
         if [[ $? != 0 ]]; then
-            echo -e "${red}下载脚本失败，请检查本机能否连接 ${Get_Docker_URL}${plain}"
+            echo -e "${red}Failed to download the script, please check whether the machine can connect ${Get_Docker_URL}${plain}"
             return 0
         fi
         systemctl enable docker.service
         systemctl start docker.service
-        echo -e "${green}Docker${plain} 安装成功"
+        echo -e "${green}Docker${plain} Successful installation"
     fi
 
     # check docker compose
     command -v docker-compose >/dev/null 2>&1
     if [[ $? != 0 ]]; then
-        echo -e "正在安装 Docker Compose"
+        echo -e "Installing Docker Compose"
         wget -t 2 -T 10 -O /usr/local/bin/docker-compose "https://${GITHUB_URL}/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" >/dev/null 2>&1
         if [[ $? != 0 ]]; then
-            echo -e "${red}下载脚本失败，请检查本机能否连接 ${GITHUB_URL}${plain}"
+            echo -e "${red}Failed to download the script, please check whether the machine can connect ${GITHUB_URL}${plain}"
             return 0
         fi
         chmod +x /usr/local/bin/docker-compose
-        echo -e "${green}Docker Compose${plain} 安装成功"
+        echo -e "${green}Docker Compose${plain} Successful installation"
     fi
 
     modify_xrayr_config 0
@@ -119,36 +119,36 @@ install() {
 }
 
 modify_xrayr_config() {
-    echo -e "> 修改xrayr配置"
+    echo -e "> Modify xrayr configuration"
 
     # download docker-compose.yml
     wget -t 2 -T 10 -O /tmp/docker-compose.yml ${DC_URL} >/dev/null 2>&1
     
     if [[ $? != 0 ]]; then
-        echo -e "${red}下载docker-compose.yml失败，请检查本机能否连接 ${DC_URL}${plain}"
+        echo -e "${red}Failed to download docker-compose.yml, please check whether the machine can connect ${DC_URL}${plain}"
         return 0
     fi
 
     # download config.yml
     wget -t 2 -T 10 -O /tmp/config.yml ${CONFIG_URL} >/dev/null 2>&1
     if [[ $? != 0 ]]; then
-        echo -e "${red}下载config.yml失败，请检查本机能否连接 ${CONFIG_URL}${plain}"
+        echo -e "${red}Failed to download config.yml, please check whether the machine can connect ${CONFIG_URL}${plain}"
         return 0
     fi
 
     # modify config.yml
     ## modify panel type
     ## choose from: SSpanel, V2board, PMpanel, Proxypanel
-    echo -e "> 请选择面板类型"
+    echo -e "> Please select panel type"
     echo -e "1. SSpanel"
     echo -e "2. V2board"
     echo -e "3. PMpanel"
     echo -e "4. Proxypanel"
     echo -e "5. NewV2board"
-    echo -e "其中 NewV2board 为xrayr v0.8.7+开始支持的新版V2board，如果您的V2board版本 >= 1.7.0，请选择NewV2board"
-    echo -e "${red}如果您的V2board版本==1.6.1，请尽快升级！${plain}"
-    echo -e "V2board 老版本API将于 2023.6.1后移除，请尽快升级"
-    read -e -p "请输入数字 [1-4]: " panel_type
+    echo -e "Among them, NewV2board is the new version of V2board supported by xrayr v0.8.7+, if your V2board version >= 1.7.0, please select NewV2board"
+    echo -e "${red}If your V2board version==1.6.1, please upgrade as soon as possible！${plain}"
+    echo -e "V2board old version API will be removed after 2023.6.1, please upgrade as soon as possible"
+    read -e -p "Please key in numbers [1-4]: " panel_type
     case $panel_type in
     1)
         sed -i "s/USER_PANEL_TYPE/SSpanel/g" /tmp/config.yml
@@ -166,18 +166,18 @@ modify_xrayr_config() {
         sed -i "s/USER_PANEL_TYPE/NewV2board/g" /tmp/config.yml
         ;;
     *)
-        echo -e "${red}请输入正确的数字 [1-4]${plain}"
+        echo -e "${red}Please enter the correct number [1-4]${plain}"
         exit 1
         ;;
     esac
     
     ## modify panel info
-    echo -e "> 修改面板域名"
-    read -e -r -p "请输入面板域名（默认：${PANEL_URL}）：" input
+    echo -e "> Modify panel domain name"
+    read -e -r -p "Please enter the panel domain name (default：${PANEL_URL}）：" input
     if [[ $input != "" ]]; then
         PANEL_URL=$input
     fi
-    read -e -r -p "请输入面板 api key（默认：${PANEL_API_KEY}）：" input
+    read -e -r -p "Please enter the panel api key (default：${PANEL_API_KEY}）：" input
     if [[ $input != "" ]]; then
         PANEL_API_KEY=$input
     fi
@@ -185,23 +185,23 @@ modify_xrayr_config() {
     PANEL_API_KEY=$(echo $PANEL_API_KEY | sed -e 's/[]\/&$*.^[]/\\&/g')
     sed -i "s/USER_PANEL_DOMAIN/${PANEL_URL}/g" /tmp/config.yml
     sed -i "s/USER_PANEL_API_KEY/${PANEL_API_KEY}/g" /tmp/config.yml
-    echo -e "> 当前域名: ${green}${PANEL_URL}${plain}"
-    echo -e "> 当前api key: ${green}${PANEL_API_KEY}${plain}"
+    echo -e "> Current domain name: ${green}${PANEL_URL}${plain}"
+    echo -e "> Current api key: ${green}${PANEL_API_KEY}${plain}"
 
     ## read NODE_ID
-    read -e -r -p "请输入节点ID（必须与面板设定的保持一致）：" input
+    read -e -r -p "Please enter the node ID (must be consistent with the panel settings)：" input
     NODE_ID=$input
-    echo -e "节点ID为: ${green}${NODE_ID}${plain}"
+    echo -e "The node ID is: ${green}${NODE_ID}${plain}"
     sed -i "s/USER_NODE_ID/${NODE_ID}/g" /tmp/config.yml
 
     ## read NODE_TYPE
     echo -e "
-    ${green}节点类型：${plain}
+    ${green}Node type：${plain}
     ${green}1.${plain}  V2ray
     ${green}2.${plain}  ShadowSocks
     ${green}3.${plain}  Trojan
     "
-    read -e -r -p "请输入选择[1-3]：" num
+    read -e -r -p "Please enter selection[1-3]：" num
     case "$num" in
     1)
         NODE_TYPE="V2ray"
@@ -213,46 +213,46 @@ modify_xrayr_config() {
         NODE_TYPE="Trojan"
         ;;
     *)
-        echo -e "${red}请输入正确的选择[1-3]${plain}"
+        echo -e "${red}Please enter the correct choice[1-3]${plain}"
         exit 1
         ;;
     esac
-    sed -i "s/USER_NODE_TYPE/${NODE_TYPE}/g" /tmp/config.yml && echo -e "成功修改节点类型为: ${green}${NODE_TYPE}${plain}"
+    sed -i "s/USER_NODE_TYPE/${NODE_TYPE}/g" /tmp/config.yml && echo -e "Successfully modified the node type to: ${green}${NODE_TYPE}${plain}"
     
 
     ## read tls
     echo -e "
-    ${green}证书申请方式：${plain}
-    ${green}1.${plain}  (none)不申请证书（如果使用nginx进行tls配置请选择此项）
-    ${green}2.${plain}  (file)自备证书文件（之后在${green}${XRAYRPATH}/XrayR/cert/${plain}目录下修改）
-    ${green}3.${plain}  (http)脚本通过http方式申请证书（需要提前解析域名到本机ip并开启80端口）
-    ${green}4.${plain}  (dns)脚本通过dns方式申请证书（脚本暂时只支持cloudflare，需要cloudflare的global api key和email）
+    ${green}How to apply for a certificate：${plain}
+    ${green}1.${plain}  (none)o not apply for a certificate（If you use nginx for tls configuration, please select this option）
+    ${green}2.${plain}  (file)Bring your own certificate file (later in${green}${XRAYRPATH}/XrayR/cert/${plain}Modify under the directory）
+    ${green}3.${plain}  (http)The script applies for a certificate through http（It is necessary to resolve the domain name to the local ip in advance and open port 80）
+    ${green}4.${plain}  (dns)The script applies for a certificate through dns（The script only supports cloudflare，need cloudflare of global api key with email）
     "
 
-    read -e -r -p "请输入选择[1-4]：" num
+    read -e -r -p "Please enter selection[1-4]：" num
     case "$num" in
     1)
-        echo -e "不申请证书"
+        echo -e "Do not apply for a certificate"
         sed -i "s/USER_CERT_MODE/none/g" /tmp/config.yml
         ;;     
     2)
-        echo -e "自备证书文件"
-        echo -e "在${green}${XRAYRPATH}/XrayR/cert/${plain}目录下修改 ${green}节点域名.cert 节点域名.key${plain}文件）"
+        echo -e "Bring your own certificate file"
+        echo -e "在${green}${XRAYRPATH}/XrayR/cert/${plain}Modify under the directory ${green}node domain name.cert node domain name.key${plain}document）"
         sed -i "s/USER_CERT_MODE/file/g" /tmp/config.yml
         TLS=true
         ;;
     3)
-        echo -e "脚本通过http方式申请证书"
+        echo -e "The script applies for a certificate through http"
         sed -i "s/USER_CERT_MODE/http/g" /tmp/config.yml
         TLS=true
         ;;
     4)
-        echo -e "脚本通过dns方式申请证书"
+        echo -e "The script applies for a certificate through dns"
         sed -i "s/USER_CERT_MODE/dns/g" /tmp/config.yml
         TLS=true
-        read -e -r -p "请输入cloudflare的global api key：" input
+        read -e -r -p "Please enter cloudflare's global api key：" input
         CLOUDFLARE_GLOBAL_API_KEY=$input
-        read -e -r -p "请输入cloudflare的email：" input
+        read -e -r -p "Please enter cloudflare's email：" input
         CLOUDFLARE_EMAIL=$input
         CLOUDFLARE_GLOBAL_API_KEY=$(echo $CLOUDFLARE_GLOBAL_API_KEY | sed -e 's/[]\/&$*.^[]/\\&/g')
         CLOUDFLARE_EMAIL=$(echo $CLOUDFLARE_EMAIL | sed -e 's/[]\/&$*.^[]/\\&/g')
@@ -260,7 +260,7 @@ modify_xrayr_config() {
         sed -i "s/USER_CLOUDFLARE_EMAIL/${CLOUDFLARE_EMAIL}/g" /tmp/config.yml
         ;;
     *)
-        echo -e "${red}输入错误，请重新输入[1-4]${plain}"
+        echo -e "${red}Input errors, please re-enter[1-4]${plain}"
         if [[ $# == 0 ]]; then
         modify_xrayr_config
         else
@@ -271,28 +271,28 @@ modify_xrayr_config() {
     esac
 
     if [ -z "${TLS}" ]; then
-        echo -e "> 不申请证书"
+        echo -e "> Do not apply for a certificate"
     else
-        read -e -r -p "请输入域名：" input
+        read -e -r -p "Please enter a domain name：" input
         NODE_DOMAIN=$input
-        echo -e "> 节点域名为: ${green}${NODE_DOMAIN}${plain}"
+        echo -e "> The node domain name is: ${green}${NODE_DOMAIN}${plain}"
         sed -i "s/USER_NODE_DOMAIN/${NODE_DOMAIN}/g" /tmp/config.yml
     fi
 
     # replace config.yml
     mv /tmp/config.yml $XRAYR_PATH/XrayR/config.yml
     mv /tmp/docker-compose.yml $XRAYR_PATH/docker-compose.yml
-    echo -e "xrayr配置 ${green}修改成功，请稍等重启生效${plain}"
+    echo -e "xrayr configuration ${green}Successfully modified，Please wait for the restart to take effect${plain}"
     # get NODE_IP
     NODE_IP=`curl -s https://ipinfo.io/ip`
     
     
     if [[ -z "${TLS}" ]]; then
-        echo -e "> 不申请证书"
+        echo -e "> Do not apply for a certificate"
     else
-        echo -e "> 节点域名为：${yellow}${NODE_DOMAIN}${plain}"
+        echo -e "> The node domain name is：${yellow}${NODE_DOMAIN}${plain}"
     fi
-    echo -e "> 节点IP为：${yellow}${NODE_IP}${plain}"
+    echo -e "> Node IP is：${yellow}${NODE_IP}${plain}"
 
     # show config
     show_config 0
@@ -306,13 +306,13 @@ modify_xrayr_config() {
 }
 
 start() {
-    echo -e "> 启动xrayr"
+    echo -e "> start xrayr"
     # start docker-compose
     cd $XRAYR_PATH && docker-compose up -d
     if [[ $? == 0 ]]; then
-        echo -e "${green}启动成功${plain}"
+        echo -e "${green}Start successfully${plain}"
     else
-        echo -e "${red}启动失败，请稍后查看日志信息${plain}"
+        echo -e "${red}Failed to start, please check the log information later${plain}"
     fi
 
     if [[ $# == 0 ]]; then
@@ -321,13 +321,13 @@ start() {
 }
 
 stop() {
-    echo -e "> 停止xrayr"
+    echo -e "> stop xrayr"
 
     cd $XRAYR_PATH && docker-compose down
     if [[ $? == 0 ]]; then
-        echo -e "${green}停止成功${plain}"
+        echo -e "${green}stop success${plain}"
     else
-        echo -e "${red}停止失败，请稍后查看日志信息${plain}"
+        echo -e "${red}Failed to stop, please check the log information later${plain}"
     fi
 
     if [[ $# == 0 ]]; then
@@ -336,15 +336,15 @@ stop() {
 }
 
 restart_and_update() {
-    echo -e "> 重启xrayr"
+    echo -e "> restart xrayr"
     cd $XRAYR_PATH
     docker-compose pull
     docker-compose down
     docker-compose up -d
     if [[ $? == 0 ]]; then
-        echo -e "${green}重启成功${plain}"
+        echo -e "${green}restart successfully${plain}"
     else
-        echo -e "${red}重启失败，请稍后查看日志信息${plain}"
+        echo -e "${red}Restart failed, please check the log information later${plain}"
     fi
 
     if [[ $# == 0 ]]; then
@@ -353,7 +353,7 @@ restart_and_update() {
 }
 
 show_log() {
-    echo -e "> 获取xrayr日志"
+    echo -e "> get xrayr log"
 
     cd $XRAYR_PATH && docker-compose logs -f
 
@@ -363,7 +363,7 @@ show_log() {
 }
 
 show_config() {
-    echo -e "> 查看xrayr配置"
+    echo -e "> View xrayr configuration"
 
     cd $XRAYR_PATH
     CONFIG_FILE="${XRAYR_PATH}/XrayR/config.yml"
@@ -381,16 +381,16 @@ show_config() {
     CLOUDFLARE_API_KEY=$(cat ${CONFIG_FILE} | grep "CLOUDFLARE_API_KEY" | awk -F ':' '{print $2}')
 
     echo -e "
-    面板类型：${green}${PANEL_TYPE}${plain}
-    前端域名：${green}${PANEL_URL}${plain}
-    api key：${green}${PANEL_API_KEY}${plain}
-    节点IP：${green}${NODE_IP}${plain}
-    节点ID：${green}${NODE_ID}${plain}
-    节点类型：${green}${NODE_TYPE}${plain}
-    证书模式：${green}${CertMode}${plain}
-    证书文件：${green}${CertFile}${plain}
-    私钥文件：${green}${KeyFile}${plain}
-    节点域名：${green}${NODE_DOMAIN}${plain}
+    Panel type：${green}${PANEL_TYPE}${plain}
+    Domain name：${green}${PANEL_URL}${plain}
+    Api key：${green}${PANEL_API_KEY}${plain}
+    Node IP：${green}${NODE_IP}${plain}
+    Node ID：${green}${NODE_ID}${plain}
+    Node type：${green}${NODE_TYPE}${plain}
+    Certificate mode：${green}${CertMode}${plain}
+    Certificate file：${green}${CertFile}${plain}
+    Private key file：${green}${KeyFile}${plain}
+    Node domain name：${green}${NODE_DOMAIN}${plain}
     Cloudflare Email：${green}${CLOUDFLARE_EMAIL}${plain}
     Cloudflare API Key：${green}${CLOUDFLARE_API_KEY}${plain}
     "
@@ -400,16 +400,16 @@ show_config() {
 }
 
 uninstall() {
-    # 确认是否卸载
-    echo -e "> 确认是否卸载xrayr"
-    read -p "请输入y/n：" confirm
+    # Confirm to uninstall
+    echo -e "> Confirm whether to uninstall xrayr"
+    read -p "please enter y/n：" confirm
     if [[ $confirm == "y" ]]; then
-        echo -e "> 卸载xrayr"
+        echo -e "> uninstall xrayr"
         cd $XRAYR_PATH && docker-compose down
         rm -rf $XRAYR_PATH
-        echo -e "${green}卸载成功${plain}"
+        echo -e "${green}uninstalled successfully${plain}"
     else
-        echo -e "${green}取消卸载${plain}"
+        echo -e "${green}cancel uninstall${plain}"
     fi
 
     if [[ $# == 0 ]]; then
@@ -418,7 +418,7 @@ uninstall() {
 }
 
 before_show_menu() {
-    echo && echo -n -e "${yellow}* 按回车返回主菜单 *${plain}" && read temp
+    echo && echo -n -e "${yellow}* Press enter to return to the main menu *${plain}" && read temp
     show_menu
 }
 
@@ -431,12 +431,12 @@ clean_all() {
 }
 
 install_bbr() {
-    # 判断是否安装了bbr
+    # Determine whether bbr is installed
     lsmod | grep bbr
     if [[ $? == 0 ]]; then
-        echo -e "${green}已安装bbr 无需安装${plain}"
+        echo -e "${green}installed bbr no installation required${plain}"
     else
-        echo -e "> 安装bbr"
+        echo -e "> install bbr"
         wget --no-check-certificate -O /opt/bbr.sh https://github.com/teddysun/across/raw/master/bbr.sh
         chmod 755 /opt/bbr.sh
         /opt/bbr.sh
@@ -446,20 +446,20 @@ install_bbr() {
 
 show_menu() {
     echo -e "
-    ${green}xrayr Docker安装管理脚本${plain}
-    ${green}1.${plain}  安装xrayr
-    ${green}2.${plain}  修改xrayr配置
-    ${green}3.${plain}  启动xrayr
-    ${green}4.${plain}  停止xrayr
-    ${green}5.${plain}  重启并更新xrayr（没有更新版本啦！）
-    ${green}6.${plain}  查看xrayr日志
-    ${green}7.${plain}  查看xrayr配置
-    ${green}8.${plain}  卸载xrayr
-    ${green}9.${plain}  安装bbr
+    ${green}xrayr Docker installation management script${plain}
+    ${green}1.${plain}  install xrayr
+    ${green}2.${plain}  Modify xrayr configuration
+    ${green}3.${plain}  start xrayr
+    ${green}4.${plain}  stop xrayr
+    ${green}5.${plain}  Restart and update xrayr (there is no newer version!)
+    ${green}6.${plain}  View xrayr logs
+    ${green}7.${plain}  View xrayr configuration
+    ${green}8.${plain}  uninstall xrayr
+    ${green}9.${plain}  install bbr
     ————————————————
-    ${green}0.${plain}  退出脚本
+    ${green}0.${plain}  exit script
     "
-    echo && read -ep "请输入选择 [0-8]: " num
+    echo && read -ep "please enter selection [0-8]: " num
 
     case "${num}" in
     0)
@@ -493,7 +493,7 @@ show_menu() {
         install_bbr
         ;;
     *)
-        echo -e "${red}请输入正确的数字 [0-8]${plain}"
+        echo -e "${red}Please enter the correct number [0-8]${plain}"
         ;;
     esac
 }
