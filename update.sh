93187d6a5dee22cefdd16a80b5578409f41cf070
ErrorX <99178547+FakeErrorX@users.noreply.github.com>
2022-12-27 23:06:29 +0600

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
-# 升级脚本
+# upgrade script
 
 xrayr_path="/opt/xrayr"
 old_config_file="$xrayr_path/config.yml"
@@ -9,31 +9,31 @@ new_config_file="$xrayr_path/XrayR/config.yml"
 
 # Check if the xrayr_path exists, if it does not exist, then exit
 if [ ! -d "$xrayr_path" ]; then
-  echo "xrayr似乎未安装，请检查后重试"
+  echo "xrayr does not appear to be installed, please check and try again"
   exit
 fi
 
 # check if old config file exists
 if [ ! -f "$old_config_file" ]; then
-  echo "xrayr似乎未安装，请检查后重试"
+  echo "xrayr does not appear to be installed, please check and try again"
   exit
 fi
 
 # check if xrayr_path directory exists docker-compose.yml
 if [ ! -f "$xrayr_path/docker-compose.yml" ]; then
-  echo "xrayr似乎未安装，请检查后重试"
+  echo "xrayr does not appear to be installed, please check and try again"
   exit
 fi
 
 # check if xrayr_path directory exists XrayR
 if [ ! -d "$xrayr_path/XrayR" ]; then
-  echo "xrayr似乎未安装完全，请检查后重试"
+  echo "xrayr seems not installed completely, please check and try again"
   exit
 fi
 
 # check if xrayr_path directory exists XrayR/config.yml
 if [ ! -f "$xrayr_path/XrayR/config.yml" ]; then
-  echo "xrayr似乎已经升级，请检查后重试"
+  echo "xrayr seems to have been upgraded, please check and try again"
   exit
 fi
 
@@ -44,7 +44,7 @@ mv $old_config_file $new_config_file
 rm -rf $old_config_file
 
 # get new docker-compose.yml
-DC_YML_URL="https://raw.githubusercontent.com/tech-fever/xrayr-docker-script/main/docker-compose.yml"
+DC_YML_URL="https://raw.githubusercontent.com/FakeErrorX/xrayr-docker-script/main/docker-compose.yml"
 wget -O $xrayr_path/docker-compose.yml $DC_YML_URL
 
 # update and restart
@@ -55,7 +55,7 @@ docker-compose up -d
 
 # check if xrayr container is running
 if [ "$(docker inspect -f {{.State.Running}} xrayr)" = "true" ]; then
-  echo "xrayr升级成功"
+  echo "xrayr upgrade successful"
 else
-  echo "xrayr升级失败，请检查后重试"
+  echo "xrayr upgrade failed, please check and try again"
 fi
